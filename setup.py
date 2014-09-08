603b65b556a06c223c8907ed4b90570308463d0a
Hugo Lopes Tavares <hltbra@gmail.com>
2014-09-08 16:47:12 -0400

    Release 1.1.0-hugo: crashmail, crashmailbatch, and fatalmailbatch have stderr_lines & stdout_lines parameters

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except (IOError, OSError):
     CHANGES = ''
 
 setup(name='superlance',
-      version='1.0.0-dev',
+      version='1.1.0-hugo',
       license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
       description='superlance plugins for supervisord',
       long_description=README + '\n\n' +  CHANGES,
